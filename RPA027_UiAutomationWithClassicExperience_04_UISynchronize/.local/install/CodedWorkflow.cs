d4f868b2277bd9be991c3f1a61acc3a13c4b5cf4
Jonatan Gomez Garcia <jonatanswdev@proton.me>
2023-12-22 03:11:43 +0900

    Module 18 Practice 2 Queues
    Build a Transaction practice based on Queue

--- RPA027_UiAutomationWithClassicExperience_04_UISynchronize/.local/install/CodedWorkflow.cs
+++ RPA027_UiAutomationWithClassicExperience_04_UISynchronize/.local/install/CodedWorkflow.cs
@@ -15,7 +15,7 @@ namespace RPA027_UiAutomationWithClassicExperience_04_UISynchronize
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
